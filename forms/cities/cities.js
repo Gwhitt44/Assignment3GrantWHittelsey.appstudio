c9e160c292476ecb0df64b856add1f5141c5e845
Grant Whittelsey <gmw92396@creighton.edu>
2021-10-03 20:54:56 -0500

    Initial commit

---
+++ forms/cities/cities.js
@@ -0,0 +1,20 @@
+/*
+let cities = []
+let i =1
+let question = confirm("Would you like to add a city? (Click OK for yes and Cancel for No)")
+
+
+while (question){
+  let newCity = prompt("Please enter the name of a City.");
+  cities.push(newCity.toLowerCase());
+  question = confirm("Would you like to enter another city?")
+  i++
+  }
+  
+  let count = 0 
+while (count < cities.length){
+  console.log(`${cities[count]}`)
+  count ++
+  }
+  
+*/
---
+++ forms/functionCompare/functionCompare.js
@@ -0,0 +1,17 @@
+/*
+let pass1 = prompt("Please enter a new password.")
+let pass2 = prompt("Please reenter your new password.")
+
+
+function compare(pass1, pass2){
+ return pass1.localeCompare(pass2);
+}
+
+let result = compare(pass1, pass2)
+if (result === 0){
+  console.log("The passwords are the same.")
+}else{
+  console.log("The passwords are not the same.")
+}
+
+*/
